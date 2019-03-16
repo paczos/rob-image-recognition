619b842e54f31def12181d4725f0482d2fbd9f3d
Pawel Paczuski <pawel.paczuski@upmedic.pl>
2019-03-16 22:57:42 +0100

    3, 4: training using reduced set, checking different parzen windows

--- lab2/mainscript.m
+++ lab2/mainscript.m
@@ -1,16 +1,15 @@
 % malutki plik do uruchomienia funkcji pdf
 load pdf_test.txt
 size(pdf_test)
-
+format longE
 % ile jest klas?
-labels = unique(pdf_test(:,1))
+labels = unique(pdf_test(:,1));
 
 % ile jest pr�bek w ka�dej klasie?
-[labels'; sum(pdf_test(:,1) == labels')]
+[labels'; sum(pdf_test(:,1) == labels')];
 		  % ^^^ dobrze by�oby pomy�le� o tym wyra�eniu
 
 % jak uk�adaj� si� pr�bki?
-plot2features(pdf_test, 2, 3)
 
 
 pdfindep_para = para_indep(pdf_test);
@@ -29,7 +28,7 @@ pdfindep_para = para_indep(pdf_test);
 %       0.19087   0.27179
 
 % teraz do zaimplementowania jest sama funkcja licz�ca pdf 
-pi_pdf = pdf_indep(pdf_test([2 7 12 17],2:end), pdfindep_para)
+pi_pdf = pdf_indep(pdf_test([2 7 12 17],2:end), pdfindep_para);
 
 %pi_pdf =
 %  1.4700e+000  4.5476e-007
@@ -39,10 +38,8 @@ pi_pdf = pdf_indep(pdf_test([2 7 12 17],2:end), pdfindep_para)
 
 % wielowymiarowy rozk�ad normalny - parametry ...
 
-pdfmulti_para = para_multi(pdf_test)
+pdfmulti_para = para_multi(pdf_test);
 
-% todo uzyć cov
-% todo mvnpdf liczy rozkład wielowymiarowy
 % pout.sig(:,:, i) = cov(....)
 
 %pdfmulti_para =
@@ -64,7 +61,7 @@ pdfmulti_para = para_multi(pdf_test)
 
 
 % ... i funkcja licz�ca g�sto��
-pm_pdf = pdf_multi(pdf_test([2 7 12 17],2:end), pdfmulti_para)
+pm_pdf = pdf_multi(pdf_test([2 7 12 17],2:end), pdfmulti_para);
 
 %pm_pdf =
 %  7.9450e-001  6.5308e-017
@@ -73,7 +70,7 @@ pm_pdf = pdf_multi(pdf_test([2 7 12 17],2:end), pdfmulti_para)
 %  4.5833e-006  2.8928e+000
 
 % parametry dla aproksymacji oknem Parzena
-pdfparzen_para = para_parzen(pdf_test, 0.5)
+pdfparzen_para = para_parzen(pdf_test, 0.5);
 									 % ^^^ szeroko�� okna
 
 %pdfparzen_para =
@@ -98,7 +95,7 @@ pdfparzen_para = para_parzen(pdf_test, 0.5)
 %    }
 %    parzenw =  0.50000
 
-pp_pdf = pdf_parzen(pdf_test([2 7 12 17],2:end), pdfparzen_para)
+pp_pdf = pdf_parzen(pdf_test([2 7 12 17],2:end), pdfparzen_para);
 
 %pp_pdf =
 %  9.7779e-001  6.1499e-008
@@ -119,11 +116,11 @@ for i=77:152:1824
 end
 
 % pierwszy rzut oka na dane
-size(train)
-size(test)
-labels = unique(train(:,1))
-unique(test(:,1))
-[labels'; sum(train(:,1) == labels')]
+%size(train)
+%size(test)
+labels = unique(train(:,1));
+unique(test(:,1));
+[labels'; sum(train(:,1) == labels')];
 
 % pierwszym zadaniem po za�adowaniu danych jest sprawdzenie,
 % czy w zbiorze ucz�cym nie ma pr�bek odstaj�cych
@@ -132,41 +129,40 @@ unique(test(:,1))
 % wy�wietlenie histogramu cech(y): hist
 % spojrzenie na dwie cechy na raz: plot2features (dostarczona w pakiecie)
 
-[mean(train); median(train)]
-hist(train(:,1))
-Combs = nchoosek(labels(2:end), 2)
-for i=1:size(Combs)
-     f1 = Combs(i, 1);
-     f2 = Combs(i, 2);
-     plot2features(train, f1, f2);
-end
+%[mean(train); median(train)];
+%hist(train(:,1));
 
 
 % do identyfikacji odstaj�cych pr�bek doskonale nadaj� si� wersje
 % funkcji min i max z dwoma argumentami wyj�ciowymi
 
 for i=2:size(train,2)
-    [mv midx] = min(train(:, i));
-    sprintf("min %d for class %d is at %d", mv,  i, midx);
+%    [mv midx] = min(train(:, i));
+%    fprintf("min %d for class %d is at %d", mv,  i, midx);
 end
 
 for i=2:size(train,2)
-    [mv midx ] = min(train(:, i))
-    sprintf("min %d for class %d is at %d", mv,  i, midx)
+%    [mv midx ] = min(train(:, i));
+%    fprintf("min %d for class %d is at %d", mv,  i, midx);
 end
 
-[mv midx] = min(train)
+%[mv midx] = min(train)
 
 % poniewa� warto�ci minimalne czy maksymalne da si� wyznaczy� zawsze,
 % dobrze zweryfikowa� ich odstawanie spogl�daj�c przynajmniej na s�siad�w
 % podejrzanej pr�bki w zbiorze ucz�cym
 
 % je�li nabra�em przekonania, �e pr�bka midx jest do usuni�cia, to:
-size(train)
+%size(train)
 train([186, 642], :) = [];
-size(train)
+%size(train)
 
-
+Combs = nchoosek(labels(2:end), 2);
+for i=1:size(Combs)
+     f1 = Combs(i, 1);
+     f2 = Combs(i, 2);
+     %plot2features(train, f1, f2);
+end
 
 % procedur� szukania i usuwania warto�ci odstaj�cych trzeba powtarza� do skutku
 
@@ -182,41 +178,64 @@ test = test(:, [1 first_idx second_idx]);
 
 % to nie jest najros�dniejszy wyb�r; 4 i 6 na pewno trzeba zmieni�
 
-% tutaj jawnie tworz� struktur� z parametrami dla klasyfikatora Bayesa 
+% tutaj jawnie tworz� struktur� z parametrami dla klasyfikatora Bayesa
 % (po prawdzie, to dla funkcji licz�cej g�sto�� prawdobie�stwa) z za�o�eniem,
 % �e cechy s� niezale�ne
 
 pdfindep_para = para_indep(train);
 pdfmulti_para = para_multi(train);
-pdfparzen_para = para_parzen(train, 0.001); 
-% w sprawozdaniu trzeba podawa� szeroko�� okna (nie liczymy tego parametru z danych!)	
+pdfparzen_para = para_parzen(train, 0.001);
+% w sprawozdaniu trzeba podawa� szeroko�� okna (nie liczymy tego parametru z danych!)
 
 % wyniki do punktu 3
+sprintf("punkt 3: ")
+fprintf("\n|cechy|pdfindep_para|pdfmulti_para|pdfparzen_para|\n")
+fprintf("--------\n")
 base_ercf = zeros(1,3);
-base_ercf(1) = mean(bayescls(test(:,2:end), @pdf_indep, pdfindep_para) != test(:,1));
-base_ercf(2) = mean(bayescls(test(:,2:end), @pdf_multi, pdfmulti_para) != test(:,1));
-base_ercf(3) = mean(bayescls(test(:,2:end), @pdf_parzen, pdfparzen_para) != test(:,1));
-base_ercf
+apriori = repmat([0.25], rows(test(:,2:end)), 1);
+base_ercf(1) = mean(bayescls(test(:,2:end), @pdf_indep, pdfindep_para, apriori) != test(:,1));
+base_ercf(2) = mean(bayescls(test(:,2:end), @pdf_multi, pdfmulti_para, apriori) != test(:,1));
+base_ercf(3) = mean(bayescls(test(:,2:end), @pdf_parzen, pdfparzen_para, apriori) != test(:,1));
+fprintf("|%d  %d|%f|%f|%f|\n", first_idx, second_idx, base_ercf(1), base_ercf(2), base_ercf(3)) % odpowiednio  sformatowany wiersz tabeli dla markdowna
 
 % W kolejnym punkcie przyda si� funkcja reduce, kt�ra redukuje liczb� pr�bek w poszczeg�lnych
 % klasach (w tym przypadku redukcja b�dzie taka sama we wszystkich klasach - ZBIORU UCZ�CEGO)
 % Poniewa� reduce ma losowa� pr�bki, to eksperyment nale�y powt�rzy� 5 (lub wi�cej) razy
 % W sprawozdaniu prosz� poda� tylko warto�� �redni� i odchylenie standardowe wsp�czynnika b��du
-
+% 4:
 parts = [0.1 0.25 0.5];
 rep_cnt = 5; % przynajmniej
 
-% YOUR CODE GOES HERE 
-% 
-
+labels = unique(train(:,1));
+fprintf("\n|czesc|powtorzenie|cechy|pdfindep_para|pdfmulti_para|pdfparzen_para|\n")
+fprintf("----------\n")
+    for p=1:columns(parts)
+        for rep=1:rep_cnt
+        tr = reduce(train, repmat([parts(p)], rows(labels), 1)');
+        pdfindep_para = para_indep(tr);
+        pdfmulti_para = para_multi(tr);
+        pdfparzen_para = para_parzen(tr, 0.001);
+        base_ercf = zeros(1,3);
+        apriori = repmat([0.25], rows(test(:,2:end)), 1);
+        base_ercf(1) = mean(bayescls(test(:,2:end), @pdf_indep, pdfindep_para, apriori) != test(:,1));
+        base_ercf(2) = mean(bayescls(test(:,2:end), @pdf_multi, pdfmulti_para, apriori) != test(:,1));
+        base_ercf(3) = mean(bayescls(test(:,2:end), @pdf_parzen, pdfparzen_para, apriori) != test(:,1));
+        fprintf("|%f|%d|%d  %d|%f|%f|%f|\n", parts(p), rep, first_idx, second_idx, base_ercf(1), base_ercf(2), base_ercf(3)) % odpowiednio  sformatowany wiersz tabeli dla markdowna
+    end
+end
 
 % Punkt 5 dotyczy jedynie klasyfikatora z oknem Parzena (na pe�nym zbiorze ucz�cym)
-
 parzen_widths = [0.0001, 0.0005, 0.001, 0.005, 0.01];
 parzen_res = zeros(1, columns(parzen_widths));
+apriori = repmat([0.25], rows(test(:,2:end)), 1);
 
-% YOUR CODE GOES HERE 
-% 
+fprintf("\n|parzen width|error|\n")
+fprintf("-------------\n")
+for w=1:columns(parzen_widths)
+        pdfparzen_para = para_parzen(tr, parzen_widths(w));
+        base_ercf = mean(bayescls(test(:,2:end), @pdf_parzen, pdfparzen_para, apriori) != test(:,1));
+        fprintf("|%f|%f|\n", parzen_widths(w), base_ercf)
+end
 
 [parzen_widths; parzen_res]
 % Tu a� prosi si� do�o�y� do danych numerycznych wykres
@@ -227,7 +246,7 @@ semilogx(parzen_widths, parzen_res)
 apriori = [0.165 0.085 0.085 0.165 0.165 0.085 0.085 0.165];
 parts = [1.0 0.5 0.5 1.0 1.0 0.5 0.5 1.0];
 
-% YOUR CODE GOES HERE 
+% YOUR CODE GOES HERE
 %
 
 
