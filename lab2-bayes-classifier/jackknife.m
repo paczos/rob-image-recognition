0c919d7f5e8f8e43661a52a1f9d2f528088d8e5d
Pawel Paczuski <pawel.paczuski@upmedic.pl>
2019-03-20 15:55:37 +0100

    clean up jackknife

--- lab2-bayes-classifier/jackknife.m
+++ lab2-bayes-classifier/jackknife.m
@@ -2,13 +2,12 @@ function errcf = jackknife(ts)
 % performs leave one test of cls1nn classifier on ts
 % ts - trainingset; first column contains class label
 % errcf = error coefficient
-res = zeros(rows(ts), 1)
+    res = zeros(rows(ts), 1);
 
-for i:rows(ts)
-res(i) = cls1nn(ts([(i-1):i (i+1):end], :), ts(i, 22:))
-end
-
-errcf = mean(clsres ~= ts(:, 1));
+    for i=1:rows(ts)
+        res(i) = cls1nn(ts([1:i-1 i+1:end], :), ts(i, 2:end));
+    end
+    errcf = mean(res ~= ts(:, 1));
 
 end
 
