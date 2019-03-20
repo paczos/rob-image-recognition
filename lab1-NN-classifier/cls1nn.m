7e7b2b6eb41fc5e0f381afec73ce015f3da4886e
Pawel Paczuski <pawel.paczuski@upmedic.pl>
2019-03-20 15:53:59 +0100

    modifications while working on lab2

--- lab1-NN-classifier/cls1nn.m
+++ lab1-NN-classifier/cls1nn.m
@@ -4,22 +4,20 @@ function label = cls1nn(ts, x)
 % x - sample to be classified (without label)
 % label - class label of x's nearest neighbour
 
-tvals = ts(:, 2:end);
-x= repmat(x, rows(ts),1)
-[min_v min_i] = min(sqrt(sum((tvals - x).^2, 2)))
-label = ts(min_i, 1)
+    tvals = ts(:, 2:end);
+    x = repmat(x, rows(ts),1);
+    [min_v min_i] = min(sqrt(sum((tvals - x).^2, 2)));
+    label = ts(min_i, 1);
 
 end
 
 function eff = evaluate(ts)
-disp("evaluating quality of the classifier");
-
-for i = 1:rows(ts)
-  curr = ts(i, :)
-  x = curr(2, 2:end)
-  rest = ts([1:i-1 i+1:end], :)
-  
-endfor
+    disp("evaluating quality of the classifier");
+    for i = 1:rows(ts)
+        curr = ts(i, :)
+        x = curr(2, 2:end)
+        rest = ts([1:i-1 i+1:end], :)
+    end
 end
 
 
