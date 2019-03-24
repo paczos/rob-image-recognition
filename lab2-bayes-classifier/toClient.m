774bf548ccf5c7350b37e22845c5fc5a0d55830f
Pawel Paczuski <pawel.paczuski@upmedic.pl>
2019-03-24 18:14:40 +0100

    info

--- lab2-bayes-classifier/toClient.m
+++ lab2-bayes-classifier/toClient.m
@@ -1,4 +1,6 @@
 function label = toClient(class)
+% class - output of a classifier trained on all classes found in data
+% label - projection of the resulting class onto the set of classes expected by the client
     tobestandardized = class-4 > 0;
     label = class;
     label(tobestandardized) = label(tobestandardized)-4;
