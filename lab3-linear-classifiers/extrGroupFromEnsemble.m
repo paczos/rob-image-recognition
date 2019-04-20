cddbd7ae1eebd19ab43b4c28c7e73aa81a74b29d
Pawel Paczuski <pawel.paczuski@upmedic.pl>
2019-04-20 16:27:47 +0200

    using logicals to improve efficiency

--- lab3-linear-classifiers/extrGroupFromEnsemble.m
+++ lab3-linear-classifiers/extrGroupFromEnsemble.m
@@ -1,11 +1,14 @@
 function extr = extrGroupFromEnsemble(ovo, group)
 
 pairs = nchoosek(1:columns(group), 2);
-extr = [];
+extr = false(rows(ovo),1);
+
 for i=1:rows(pairs)
     f = pairs(i, 1);
     s = pairs(i, 2);
-    extr = [extr; ovo(ovo(:, 1) == group(f) & ovo(:, 2)==group(s), :)];
-    extr
+    extr |=  ovo(:, 1) == group(f) & ovo(:, 2)==group(s);
 end
+
+extr = ovo(extr, :)
+
 end
