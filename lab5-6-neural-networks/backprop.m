52daf8a30be9a7646a4752a227ffbf36f7b847fd
Pawel Paczuski <pawel.paczuski@upmedic.pl>
2019-05-08 18:20:11 +0200

    sketch of backprop algorithm

--- lab5-6-neural-networks/backprop.m
+++ lab5-6-neural-networks/backprop.m
@@ -20,19 +20,30 @@ function [hidlw outlw terr] = backprop(tset, tslb, inihidlw, inioutlw, lr)
 % foreach sample in the training set
 	for i=1:rows(tset)
 		% 3. Set desired output of the ANN
-		
+		desiredOut = zeros(1, columns(outlw));
+		desiredOut(tslb(i)) = 1;
+
 		% 4. Propagate input forward through the ANN
 		% remember to extend input [tset(i, :) 1]
+		hiddenLayerOut = actf([tset(i, :) 1] * hidlw);
+		outLayerOut = actf([hiddenLayerOut 1] * outlw);
 
 		% 5. Adjust total error (just to know this value)
-		
+        sampleError = (desiredOut-outLayerOut).^2;
+		terr += sampleError;
 		% 6. Compute delta error of the output layer
 		% how many delta errors should be computed here?
-		
+        outLayerDelta = (desiredOut - outLayerOut) .* actdf(outLayerOut);
+        outLayerAdjustment = [hiddenLayerOut 1]' * outLayerDelta * lr;
+
 		% 7. Compute delta error of the hidden layer
 		% how many delta errors should be computed here?
-		
+		hiddenLayerDelta = actdf(hiddenLayerOut) * (outLayerDelta * outlw)
+		hiddenLayerAdjustment = [tset(i) 1]' * hiddenLayerDelta * lr;
+
 		% 8. Update output layer weights
-		
+		outlw += outLayerAdjustment;
+
 		% 9. Update hidden layer weights
+		hidlw += hiddenLayerAdjustment;
 	end
