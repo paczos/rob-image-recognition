4397788767d1bd979bdbd66749321c733f1d3a00
Pawel Paczuski <pawel.paczuski@upmedic.pl>
2019-05-13 16:07:17 +0200

    using correct power operator

--- lab5-6-neural-networks/actdf.m
+++ lab5-6-neural-networks/actdf.m
@@ -1,5 +1,5 @@
 function res = actdf(sfvalue)
 % derivative of sigmoid activation function
 % sfvalue - value of sigmoid activation function (!)
-    res = - exp(sfvalue) ./ pow((1+exp(sfvalue)),2);
+    res = - exp(sfvalue) ./ ((1+exp(sfvalue)).^2);
 end
