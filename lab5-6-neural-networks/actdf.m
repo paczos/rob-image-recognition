cc901c4b623074c4403c29ed24d2f45187a21c33
Pawel Paczuski <pawel.paczuski@upmedic.pl>
2019-05-13 18:24:43 +0200

    fixed derivative

--- lab5-6-neural-networks/actdf.m
+++ lab5-6-neural-networks/actdf.m
@@ -1,5 +1,4 @@
 function res = actdf(sfvalue)
 % derivative of sigmoid activation function
 % sfvalue - value of sigmoid activation function (!)
-    res = - exp(sfvalue) ./ ((1+exp(sfvalue)).^2);
-end
+  res = sfvalue .* (1 - sfvalue);
